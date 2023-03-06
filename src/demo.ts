7ccbd6479a6903a1b0316785989852250b8b1591
kemaltf <kemaltaufikf@gmail.com>
2023-03-06 14:23:23 +0800

    define type as any (method 2)

--- src/demo.ts
+++ src/demo.ts
@@ -3,9 +3,9 @@ let y: string;
 let z: boolean;
 let a: Date;
 // you can define as any in here
-let b: any;
+let b: string[];
 
 // or you can define "as any" in here
-b = "Hello!";
-b = 1234;
+b = "Hello!" as any;
+b = 1234 as any;
 z = true;
