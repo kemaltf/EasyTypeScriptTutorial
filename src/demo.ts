88f636dafd1b8da45a275320ebdb4f0c2bfe7b45
kemaltf <kemaltaufikf@gmail.com>
2023-03-06 14:28:41 +0800

    If we have two interface, we can merge into one interface using extends

--- src/demo.ts
+++ src/demo.ts
@@ -1,11 +1,23 @@
-interface Contact {
+interface Contact extends Address {
   id: number;
   name: string;
   birthDate?: Date;
 }
 
-// if we decided to not filled the birthDate, we can add "?" on the property
+interface Address {
+  line1: string;
+  line2: string;
+  province: string;
+  region: string;
+  postalcode: number;
+}
+
 let primaryContact: Contact = {
   id: 123,
   name: "kemal",
+  line1: "your address",
+  line2: "your address",
+  province: "your province",
+  region: "your region",
+  postalcode: 12345,
 };
